01162c956e640b98fc2d686a4fd1db980a988341
mick roman <mickroman65@gmail.com>
2018-12-18 00:57:47 +0000

    Used  Array.isArray, Array.from, Array.from(titles).forEach

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
-const titles = document.getElementsByClassName('title');
-
+var titles = getElementsByClassName('title');
 console.log(Array.isArray(titles));
 console.log(Array.isArray(Array.from(titles)));
 
-Array.from(titles).forEach(function(title){
-  console.log(title);
+Array.from(titles).forEach(function(item) {
+    console.log(item);
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const titles = document.getElementsByClassName('title');
+
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles)));
+
+// Array.from(titles).forEach(function(title){
+//   console.log(title);
+// });
