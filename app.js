478821dcfc766a17423ef7cb7c9fe5ff6f1e0301
mick roman <mickroman65@gmail.com>
2018-12-19 00:20:58 +0000

    version6.0

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-var books= document.querySelectorAll('#book-list');
+const banner= document.querySelector('#page-banner');
 
-Array.from(books).forEach(function(book){
-  
-  book.textContent+= "book title"  // console.log(book.textContent);
-})
-const bookList =document.querySelector('#book-list');
-//bookList.innerHTML= '<h2>Books and more books...</h2>';
-bookList.innerHTML+='<p>This is how you add HTML</p>';
+console.log('#page-banner node type is:',banner.nodeType);
+console.log('#page-banner node name is:',banner.nodeName);
+console.log('#page-banner has child nodes:',banner.hasChildNodes());
 
+const clonedBanner= banner.cloneNode(true);
+console.log(clonedBanner);
 
 
 
@@ -21,11 +19,11 @@ bookList.innerHTML+='<p>This is how you add HTML</p>';
 
 
 
+// const banner = document.querySelector('#page-banner');
 
-// Array.from(books).forEach(function(book){
-//   book.textContent += ' (Book title)';
-// });
+// console.log('#page-banner node type is:', banner.nodeType);
+// console.log('#page-banner node name is:', banner.nodeName);
+// console.log('#page-banner has child nodes:', banner.hasChildNodes());
 
-// const bookList = document.querySelector('#book-list');
-// bookList.innerHTML = '<h2>Books and more books...</h2>';
-// bookList.innerHTML += '<p>This is how you add HTML content</p>';
+// const clonedBanner = banner.cloneNode(true);
+// console.log(clonedBanner);
