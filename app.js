956b092a1aa05617c27de006d2951fa346a4e80a
mick roman <mickroman65@gmail.com>
2018-12-20 20:32:39 +0000

    version9.0

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
-const bookList= document.querySelector('#book-list');
+var btns=document.querySelectorAll('#book-list.delete');
 
-console.log('book-list next sibling is:',bookList.nextSibling);
+Array.from(btns).forEach(function(btn){
+  btn.addEventListener('click'.function(e){
+    
+     const li=e.target.parentElement;
+     li.parentNode.removeChild(li)
+  });
+  
+});
 
-console.log('book-list next element sibling is:',bookList.nextElementSibling);
-console.log('book-list previous element sibling is:',bookList.previousElementSibling);
+cost link= document.querySelector('#page-banner a');
 
-bookList.previousElementSibling.querySelector("p").innerHTML+='<br/>Too cool for everyone else!';
-
-
-
-
+link.addEventListener('click'.function(e){
+  e.preventDefault();
+  console.log('navigation to'.e.target.textContent.'was prevented');
+});
 
 
 
 
 
-// const bookList = document.querySelector('#book-list');
 
-// console.log('#book-list next sibling:', bookList.nextSibling);
-// console.log('#book-list next element sibling:', bookList.nextElementSibling);
-// console.log('#book-list previous sibling:', bookList.previousSibling);
-// console.log('#book-list previous element sibling:', bookList.previousElementSibling);
 
-// bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
+
+
+
+
+
+// const listItems = document.querySelectorAll('#book-list ul li');
+
+// Array.from(listItems).forEach(function(item){
+//   item.addEventListener('click', (e) => {
+
+//     const li = e.target.parentElement;
+//     console.log('child element to remove:', li);
+//     console.log('parent element to remove child from:', li.parentElement);
+//     li.parentNode.removeChild(li);
+
+//   });
+// });
+
+// prevent default behaviour
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', function(e){
+  e.preventDefault();
+  console.log('Navigation to', e.target.textContent, 'was prevented');
+});
